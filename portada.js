1655a04dc3d42bfee091f764db6edb4212640fa0
Yeray Deza <yedi261>
2020-01-31 11:11:47 +0100

    Cambios 31/01/2020

--- portada.js
+++ portada.js
@@ -1,8 +1,22 @@
 var photos = document.querySelector('#photos');
 var info = document.querySelector('#info');
+
 function takePhotos() {
-    for (let i = 0; i < 9; i++) {
-        var img = document.createElement('img');
-        img.setAttribute("src", "Images/Wood3");
-    }
+    console.log('entra');
+    let request = new XMLHttpRequest();
+    request.open('GET', 'https://picsum.photos/v2/list?page=2&limit=100');
+    request.responseType = 'json';
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            const response = request.response;
+            const arrayPhotos = response.download_url;
+            for (let i = 10; i > 0; i--) {
+                let photo = document.createElement('img');
+                photos.appendChild(photo);
+                console.log(response[i].download_url);
+                photo.setAttribute("src", response[i].download_url);
+            }
+        }
+    };
+    request.send();
 }
