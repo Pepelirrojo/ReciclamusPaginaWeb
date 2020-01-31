76523cce77a07b7c73c175b3c878003e60c5f04f
Pepe <jose.mateo@live.u-tad.com>
2020-01-31 09:52:03 +0100

    Root Commit

---
+++ portada.js
@@ -0,0 +1,8 @@
+var photos = document.querySelector('#photos');
+var info = document.querySelector('#info');
+function takePhotos() {
+    for (let i = 0; i < 9; i++) {
+        var img = document.createElement('img');
+        img.setAttribute("src", "Images/Wood3");
+    }
+}
