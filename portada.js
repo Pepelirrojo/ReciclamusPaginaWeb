0d7dd6de02905455255cadf592c254e1bad437af
Pepe <jose.mateo@live.u-tad.com>
2020-02-09 21:01:44 +0100

    Botones terminados y mapa añadidio.

--- portada.js
+++ portada.js
@@ -5,9 +5,11 @@ var content2 = document.querySelector('#content2');
 var btn1 = document.querySelector('#btn1');
 var btn2 = document.querySelector('#btn2');
 let request = new XMLHttpRequest();
+var btnN = document.querySelector('#btnN')
+var btnP = document.querySelector('#btnP')
 request.open('GET', 'https://picsum.photos/v2/list?page=2&limit=100');
 request.responseType = 'json';
-var j = 12;
+var j = 11;
 
 function takePhotos() {
     request.onreadystatechange = function () {
@@ -39,12 +41,13 @@ function showMeetUs() {
 
 function showWhereAre() {
     content1.innerHTML = "";
-    var text2 = document.createElement('p');
+    var text2 = document.createElement('iframe');
+    text2.setAttribute('src', 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d24270.346316226587!2d-4.252589991288267!3d40.50195382872257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd41a89d3dab4d99%3A0xc7af96853c089fc8!2s28294%20Robledo%20de%20Chavela%2C%20Madrid!5e0!3m2!1ses!2ses!4v1581277030407!5m2!1ses!2ses');
+    console.log(text2);
     content2.appendChild(text2);
     text2.setAttribute('id', 'text2');
     content2.style.background = '#b27bb3';
     content2.style.transition = '1s';
-    text2.innerHTML = "Somos una pequeña empresa que se dedica a la restauración y deccoración de muebles antiguos.";
     btn2.disabled = true;
     btn1.disabled = false;
 }
@@ -52,10 +55,30 @@ function showWhereAre() {
 function next() {
     const responseAux = request.response;
     let photoAux = document.querySelectorAll('img');
-    for (let i = 0; i < i + 12; i++
-    ) {
-        console.log(photoAux);
-        photoAux[i].src = responseAux[i + j].download_url;
+    if (j < 95) {
+        for (let i = 0; i < 12; i++
+        ) {
+            photoAux[i].src = responseAux[i + j].download_url;
+        }
+        j += 12;
     }
-    j += 12;
+    if (j >= 95) {
+        j = 11;
+    }
+}
+
+function previous() {
+    const responseAux = request.response;
+    let photoAux = document.querySelectorAll('img');
+    if (j <= 11) {
+        j = 96;
+    }
+    if (j !== 11) {
+        for (let i = 0; i < 12; i++
+        ) {
+            photoAux[i].src = responseAux[j - i].download_url;
+        }
+        j -= 12;
+        console.log(j);
+    }
 }
